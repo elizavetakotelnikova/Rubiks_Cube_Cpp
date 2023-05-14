e8dd3f9c55b4bd69b50e6b5f40023d8c8ca5eef3
Elizaveta Kotelnikova <113019328+elizavetakotelnikova@users.noreply.github.com>
2023-05-14 13:27:25 +0300

    Update Algo.h

--- Algo.h
+++ Algo.h
@@ -1,9 +1,14 @@
 #include "Cube.h"
 #include <vector>
+#include <Queue>
 class Algo {
 private:
     int stage;
 public:
     bool isCross(COLOR color, int index, Cube& cube);
     bool isRightWhiteCross(COLOR color, int index, Cube& cube);
+    bool matchCenter(COLOR a, COLOR center);
+    void SetEdge(COLOR fc);
+    void SetEdgeFront(COLOR fc);
+    void makeWhiteCross(Cube& cube);
 };
