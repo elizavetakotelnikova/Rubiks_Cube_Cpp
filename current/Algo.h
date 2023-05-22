913d7d6cf09eb0d45d90d8fa4f48ac7bdf94a54e
Anastasia <113058066+nastyxxaavs@users.noreply.github.com>
2023-05-22 22:40:10 +0300

    Update Algo.h
    rename my functions

--- current/Algo.h
+++ current/Algo.h
@@ -12,8 +12,13 @@ public:
     void SetEdgeFront(COLOR fc, Cube& Cube, COLOR ce);
     void makeWhiteCross(Cube& cube);
 
-    void phase_one(Cube &cube, Cubie &mini_cube);
-    void phase_three(Cube &Cube, Cubie &mini_cube);
+    //void phase_one(Cube &cube, Cubie &mini_cube);
+    //void phase_three(Cube &Cube, Cubie &mini_cube);
+    void First_layer_solver(Cube &Cube, COLOR color);
+    bool isYellowCross(COLOR color,Cube &Cube);
+    void Yellow_cross_solver(Cube &Cube, COLOR color);
+    int Max_corners_sum(Cube & Cube);
+    void Corner_cubies_solver(Cube& Cube);
 
     void MiddleLayer(Cube &Cube);
     void fixWrongSecondLayerEdges(COLOR_cube current_cube, Cube &Cube);
