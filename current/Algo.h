ae86ce33fccc7bc1e48d4f8a2c00ae55a5ec2247
Elizaveta Kotelnikova <113019328+elizavetakotelnikova@users.noreply.github.com>
2023-05-22 11:23:52 +0300

    Update Algo.h

--- current/Algo.h
+++ current/Algo.h
@@ -14,4 +14,8 @@ public:
 
     void phase_one(Cube &cube, Cubie &mini_cube);
     void phase_three(Cube &Cube, Cubie &mini_cube);
+
+    void MiddleLayer(Cube &Cube);
+    void fixWrongSecondLayerEdges(COLOR_cube current_cube, Cube &Cube);
+    bool isSolvedMiddleLayer(COLOR_cube &cube);
 };
