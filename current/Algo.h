ff2fb80b270a36c4694d70099aff5184ba6929bb
Elizaveta Kotelnikova <113019328+elizavetakotelnikova@users.noreply.github.com>
2023-05-23 00:27:39 +0300

    Update Algo.h

--- current/Algo.h
+++ current/Algo.h
@@ -14,11 +14,11 @@ public:
 
     //void phase_one(Cube &cube, Cubie &mini_cube);
     //void phase_three(Cube &Cube, Cubie &mini_cube);
-    void First_layer_solver(Cube &Cube, COLOR color);
+    void firstLayerSolver(Cube &Cube, COLOR color);
     bool isYellowCross(COLOR color,Cube &Cube);
-    void Yellow_cross_solver(Cube &Cube, COLOR color);
-    int Max_corners_sum(Cube & Cube);
-    void Corner_cubies_solver(Cube& Cube);
+    void yellowCrossSolver(Cube &Cube, COLOR color);
+    int maxCornersSum(Cube & Cube);
+    void cornerCubiesSolver(Cube& Cube);
 
     void MiddleLayer(Cube &Cube);
     void fixWrongSecondLayerEdges(COLOR_cube current_cube, Cube &Cube);
