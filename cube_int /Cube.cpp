366565fa24e3d84f1270d87a525702f70c1d1692
Elizaveta Kotelnikova <113019328+elizavetakotelnikova@users.noreply.github.com>
2023-05-23 22:28:24 +0300

    Update Cube.cpp

--- cube_int /Cube.cpp
+++ cube_int /Cube.cpp
@@ -1,25 +1,139 @@
 #include "cube.h"
 
 Cube::Cube() {
-    for (int i = 0; i++; i < 6) {
-        for (int j = 0; j++; j < 3) {
-            for (int k = 0; k++; k < 3) {
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
                 cube[i][j][k] = i;
             }
         }
     }
 }
 
-void Cube::Read(const char* filename) {
+uint8_t Cube::inputToInt(char x) {
+    switch(x) {
+        case "Y": return 0;
+        case "W": return 1;
+        case "G": return 2;
+        case "R": return 3;
+        case "B": return 4;
+        case "O": return 5;
+    }
+}
+
+char Cube::outputToChar(uint8_t x) {
+    switch(x) {
+        case 0: return "Y";
+        case 1: return "W";
+        case 2: return "G";
+        case 3: return "R";
+        case 4: return "B";
+        case 5: return "O";
+    }
+}
+
+std::string Cube::printColor(uint8_t x) {
+    switch(x) {
+        case 0: return "Yellow face";
+        case 1: return "White face";
+        case 2: return "Green face";
+        case 3: return "Right face";
+        case 4: return "Back face";
+        case 5: return "Orange face";
+    }
+}
+
+void Cube::Shuffle() {
+    //rotationHistory.clear(); //у нас же нет rotation history? зачем это делать, надо ли, как думаешь?
+    for (int i = 0; i < 50; i++) {
+        int random_value = abs(rand() % 18);
+        Rotate(random_value);
+    }
+}
+
+void Cube::printCube() {
+    for (int i = 0; i < 6; i++) {
+        std::cout << printColor(i) << "\n";
+        std::cout << "         ";
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                std::cout << "  ";
+            }
+            std::cout << "\n";
+        }
+        std::cout << '\n';
+    }
+}
+
+/*void printRotations() {
+    for (int i = 0; i < rotationHistory.size(); i++) {
+        if (i % 10 == 9)
+            cout << '\n';
+        cout << outputRotations[rotationHistory[i]] << ' ';
+    }
+}*/
+
+void Cube::Write(const std::string& filename) {
+    std::ofstream File;
+    File.open(filename);
+    if (File.is_open()) {
+        for (int i = 0; i < 6; i++) {
+            File << "---\n";
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    File << outputToChar(cube[i][j][k]);
+                }
+                File << '\n';
+            }
+        }
+        File << "---\n";
+    }
+    File.close();
+};
+
+/*void readFromFile(const string& fileName) {
+    ifstream file;
+    file.open(fileName);
+    std::string line;
+    if (file.is_open()) {
+        int color = 0;
+        int row = -1;
+        while (std::getline(file, line)) {
+            if (line == "---") {
+                continue;
+            } else {
+                row += 1;
+                for (int i = 0; i < line.length(); i++) {
+                    faces[color][row][i] = colorToInt(line[i]);
+                }
+                if (row == 2) {
+                    row = -1;
+                    color++;
+                }
+            }
+
+        }
+    }
+    file.close();
+}*/
+
+void Cube::Read(const std::string& filename) {
     std::ifstream File;
     File.open(filename, std::ios::in);
-    for (int i = 0; i < 8; i++) {
-        File >> cp[i] >> co[i];
+    std::string str;
+    if (File.is_open()) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3;) {
+                std::getline(File, str);
+                cube[i][j][0] = inputToInt(str[0]);
+                cube[i][j][1] = inputToInt(str[1]);
+                cube[i][j][2] = inputToInt(str[2]);
+            }
+        }
+        std::getline(File, str);
     }
-    for (int i = 0; i < 12; i++) {
-        File >> ep[i] >> eo[i];
-    }
-    std::cout << "Cube's state was successfully read from the file " << std::endl;
+    std::cout << "Cube's state was successfully read from the file" << std::endl;
+    File.close();
 }
 
 void Cube::Generate() {
@@ -32,14 +146,15 @@ void Cube::Generate() {
     }
 }
 
-bool Cube::Check() {
-    for (int i = 0; i < 8; i++) {
-        if ((cp[i] != i) or (co[i] != 0))
-            return false;
-    }
-    for (int i = 0; i < 12; i++) {
-        if ((ep[i] != i) or (eo[i] != 0))
-            return false;
+bool Cube::isSolved() {
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                if (cube[i][j][k] != i) {
+                    return false;
+                }
+            }
+        }
     }
     return true;
 }
