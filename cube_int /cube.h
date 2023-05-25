f953469502d3cf8588bda0f584fe57764ea3e28d
Elizaveta Kotelnikova <113019328+elizavetakotelnikova@users.noreply.github.com>
2023-05-25 16:27:24 +0300

    Update cube.h

--- cube_int /cube.h
+++ cube_int /cube.h
@@ -25,16 +25,16 @@
  *   the way we are holding rubics cube: white face if at the bottom (1), yellow face is top (0), green face is front (2),
  *   red face is left (3), blue face is back (4) and orange face is right (5)
 */
-enum class FACE: uint8_t {UP, LEFT, FRONT, RIGHT, BACK, DOWN};
-enum COMMANDS: uint8_t {U, Uprime, U2, F, Fprime, F2, L, Lprime, L2, R, Rprime, R2, B, Bprime, B2, D, Dprime, D2};
-enum COLOR : uint8_t {YELLOW, WHITE, GREEN, RED, BLUE, ORANGE};
+enum class FACE: int {UP, LEFT, FRONT, RIGHT, BACK, DOWN};
+enum COMMANDS: int {U, Uprime, U2, F, Fprime, F2, L, Lprime, L2, R, Rprime, R2, B, Bprime, B2, D, Dprime, D2};
+enum COLOR : int {YELLOW, WHITE, GREEN, RED, BLUE, ORANGE};
 enum CORNER {ULB, URB, URF, ULF, DLF, DLB, DRB, DRF}; //position is a solved state
 enum CORNERS_COLOR {YRB, YOB, YOG, YRG, WRG, WRB, WOB, WOG};
 enum EDGE {UB, UR, UF, UL, FR, FL, BL, BR, DF, DL, DB, DR}; // position in s solved state
 enum EDGES_COLOR {YB, YO, YG, YR, GO, GR, BLUERED, BO, WG, WR, WB, WO};
 
-typedef uint8_t (*COLOR_cube)[3][3];
-typedef uint8_t (*face_cube)[3];
+typedef int (*COLOR_cube)[3][3];
+typedef int (*face_cube)[3];
 
 /*class Cubie {
 private:
@@ -61,7 +61,8 @@ struct Face {
 
 class Cube {
 private:
-    uint8_t cube[6][3][3];
+    int cube[6][3][3];
+    std::vector<std::string> history;
     //std::string cornerNames[8] = {"URF", "UBR", "DLF", "DFR", "ULB", "UFL", "DRB", "DBL"};
     //std::string edgeNames[12] = {"UF", "UR", "UB", "UL", "DF", "DR", "DB", "DL", "FR", "BR", "BL", "FL"};
 public:
@@ -70,10 +71,11 @@ public:
     void Shuffle();
     void Write(const std::string& filename);
     void printCube();
+    void printRotations();
 
     uint8_t inputToInt(char x);
-    char outputToChar(uint8_t x);
-    std::string printColor(uint8_t x);
+    char outputToChar(int x);
+    std::string printColor(int x);
     /*COLOR getCubieRight(int side, int row, int column);
     COLOR getCubieLeft(int side, int row, int column);
     COLOR getCubieTop(int side, int row, int column);*/
