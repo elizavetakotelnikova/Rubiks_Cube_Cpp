f5fdeb4b3e186d64b5e96a95e4b55a76dbbbba6e
Elizaveta Kotelnikova <113019328+elizavetakotelnikova@users.noreply.github.com>
2023-06-01 15:42:44 +0300

    Update Algo.h

--- cube_final/Algo.h
+++ cube_final/Algo.h
@@ -5,8 +5,10 @@ class Algo {
 private:
     int stage;
 public:
+    //solving functions
     void Solve(Cube& Cube);
 
+    //White Cross Solver
     bool isCross(COLOR color, int index, Cube& cube);
     bool isRightWhiteCross(Cube& cube);
     bool matchCenter(int a, int center);
@@ -14,28 +16,33 @@ public:
     void SetEdgeFront(int fc, Cube& Cube, int ce);
     void makeWhiteCross(Cube& cube);
 
+    //First layer Solver
     void firstLayerSolver(Cube &Cube);
     bool isSolvedFirstLayer(Cube &cube);
 
-    void middleLayer(Cube &Cube);
+    //Middle layer Solver
+    void solveMiddleLayer(Cube &Cube);
     void fixWrongSecondLayerEdges(COLOR_cube current_cube, Cube &Cube);
     bool isSolvedMiddleLayer(COLOR_cube &cube);
-
-    bool isYellowCross(Cube &cube);
-    void yellowCrossSolver(Cube &Cube);
-
-    void solveYellowFace(Cube &Cube);
-    bool isSolvedYellow(Cube &Cube);
-
-    int maxCornersSum(Cube & Cube);
-    void cornerCubiesSolver(Cube& Cube);
-    bool highCornersCorrectPos(Cube &Cube);
-
-    void solveCorners(Cube &Cube);
-    bool areCornersSolved(Cube &Cube);
-
     void solveOrange(Cube &Cube);
     void solveRed(Cube &Cube);
     void solveBlue(Cube &Cube);
     void solveGreen(Cube &Cube);
+
+    //Yellow Cross Solver
+    bool isYellowCross(Cube &cube);
+    void solveYellowCross(Cube &Cube);
+
+    //Yellow face solver
+    void solveYellowFace(Cube &Cube);
+    bool isSolvedYellow(Cube &Cube);
+
+    //Up face's corner cubies solver
+    int maxCornersSum(Cube & Cube);
+    void solveCornerCubies(Cube& Cube);
+    bool highCornersCorrectPos(Cube &Cube);
+
+    //Up face's edges solver
+    void solveEdges(Cube &Cube);
+    bool areEdgesSolved(Cube &Cube);
 };
